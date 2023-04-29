b9bbdb39ef62d1aec0da56933f27037005899258
Roldao Araujo <roldao.wilker@gmail.com>
2023-04-30 00:25:01 +0100

    Started base for about and terms pages

---
+++ oportunidades/mia/app.js
@@ -0,0 +1,12 @@
+setTimeout(3000,
+  //window.location.href = "https://secure.doppus.com/go/G8889E9Z3J3E53JO"
+)
+
+document.onkeydown = function (evt) {
+  evt = evt || window.event;
+  if (evt.keyCode == 27) {
+    evt.preventDefault();
+  }
+};
+
+
